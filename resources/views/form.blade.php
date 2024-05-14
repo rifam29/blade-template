38715853ed6d14373a96b3ea5172a029a01cd406
Rifam29 <rifamazharulhaq.dhp@gmail.com>
2024-05-14 13:40:17 +0700

    blade-templating

---
+++ resources/views/form.blade.php
@@ -0,0 +1,43 @@
+@extends('template.master')
+
+@section('content')
+<div class="container-fluid pt-4 px-4">
+                <div class="row g-5">
+                    <div class="col-sm-12 col-xl-6">
+                        <div class="bg-secondary rounded h-100 p-4">
+                            <h6 class="mb-4">Floating Label</h6>
+                            <form>
+                                <div class="form-floating mb-3">
+                                    <input type="email" class="form-control" id="floatingInput"
+                                    placeholder="name@example.com">
+                                <label for="floatingInput">Email address</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control" id="floatingPassword"
+                                    placeholder="Password">
+                                <label for="floatingPassword">Password</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <select class="form-select" id="floatingSelect"
+                                    aria-label="Floating label select example">
+                                    <option selected>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                                <label for="floatingSelect">Works with selects</label>
+                            </div>
+                            <div class="form-floating">
+                                <textarea class="form-control" placeholder="Leave a comment here"
+                                    id="floatingTextarea" style="height: 150px;"></textarea>
+                                <label for="floatingTextarea">Comments</label>
+                            </div>
+                            <button type="submit" class="btn btn-primary mt-3">Sign in</button>
+                        </form>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-xl-6">
+                    </div>
+                </div>
+            </div>
+@endsection
